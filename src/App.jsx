f5bc4232f0539a8d0385ade9db8ea48efeec729c
babyboy198331-coder <babyboy198331@gmail.com>
2026-04-08 17:18:26 -0500

    master

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import Todo from './components/Todo.jsx';
  import Title from './components/Title.jsx';
+import Modal from './components/modal.jsx';
 
 
 function App() {
@@ -8,11 +9,16 @@ function App() {
     <div>
 <Title></Title>
 <div className="Todo__wrapper">
-<Todo Title="Finsh Frontend simplified"/>
-<Todo Title="Finish Interview Section"/>
-<Todo Title="Land a $100k Job"/>
+<Todo Title="Finsh Frontend simplified"
+      paragraph="Code along with Frontend Simplified step by step" 
+/>
+<Todo Title="Finish Interview Section"
+      paragraph="Finish every interview question in the next 6 wweks" 
+/>
+<Todo Title="Land a $100k Job" paragraph="Apply to 100 jobs"/>
 </div>
- {/* <Modal /> */}
+ {/* <Modal title="Are you sure you want to delete?"/> */}
+ <Modal title="Are you sure you want to Add?"/>
     </div>
  
   );
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,10 @@
 import './Todo.css';
 
-function Todo({ Title }) {
+function Todo({ Title, paragraph }) {
     return (
         <div className="Todo">
         <h2>{Title}</h2>
+        <p>{paragraph}</p>
         <button>Delete</button>
       </div>
     )
