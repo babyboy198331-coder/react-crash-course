a46d0660249417f70127c04f6ca4eb5358d90f6f
babyboy198331-coder <babyboy198331@gmail.com>
2026-04-09 12:38:01 -0500

    master

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,39 @@
 import './App.css';
 import Todo from './components/Todo.jsx';
- import Title from './components/Title.jsx';
-import Modal from './components/modal.jsx';
-
+import Title from './components/Title.jsx';
+import Modal from './components/Modal.jsx';
+import Counter from './components/Counter.jsx';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-   <div>
-<Title />
-<div>
-  <input type="text" onChange={()=> {
+  const [showModal, setShowModal] = useState(false);
 
-  }}/>
-  <button>Add Todo</button>
-</div>
-<div className="Todo__wrapper">
-<Todo Title="Finsh Frontend simplified"/>
-<Todo Title="Finish Interview Section"/>
-<Todo Title="Land a $100k Job"/>
-</div>
-  <Modal title="Are you sure you want to delete?"/>
- 
+  return (
+    <div>
+      <Title />
+
+      <Counter />
+
+      <div>
+        <input
+          type="text"
+          onChange={(event) => {
+            // handle input here
+          }}
+        />
+        <button onClick={() => setShowModal(true)}>
+          Add Todo
+        </button>
+      </div>
+
+      <div className="Todo__wrapper">
+        <Todo title="Finish Frontend Simplified" />
+        <Todo title="Finish Interview Section" />
+        <Todo title="Land a $100k Job" />
+      </div>
+
+      {showModal && <Modal title="Confirm Delete?" />}
     </div>
- 
   );
 }
 
