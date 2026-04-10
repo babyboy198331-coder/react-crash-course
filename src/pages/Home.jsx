715402157179d4e11fecb1c2c43d863ff6f3ccdf
babyboy198331-coder <babyboy198331@gmail.com>
2026-04-10 17:10:40 -0500

    master

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,10 @@
+import React, {useEffect} from 'react'
+import axios from 'axios'
+
 function Home() {
+    useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/usres")
+    }, [])
     return <h1>Home</h1>;
 }
 
