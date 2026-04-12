80e5a3bce68b183925daa8ab243628a3a11fdde6
babyboy198331-coder <babyboy198331@gmail.com>
2026-04-12 13:38:02 -0500

    master

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,34 +1,54 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import User from '../components/User';
 
 function Home() {
   const [users, setUsers] = useState([]);
 
   async function fetchUsers() {
     const { data } = await axios.get(
-      "https://jsonplaceholder.typicode.com/users"
+      "https://jsonplaceholder.typicode.com/users/id"
     );
     setUsers(data);
   }
 
   useEffect(() => {
-    setTimeout(() => {
+    const timer = setTimeout(() => {
       fetchUsers();
     }, 500);
+
+    return () => clearTimeout(timer);
   }, []);
 
-  const pixels = "3px";
+  function renderUsers() {
+    return users.map((user) => (
+      <Link
+        to={`/users/${user.id}`}
+        key={user.id}
+        style={{
+          display: "block",
+          textDecoration: "none",
+          color: "inherit"
+        }}
+      >
+        <User
+          id={user.id}
+          name={user.name}
+          email={user.email}
+          username={user.username}
+        />
+      </Link>
+    ));
+  }
+
+  function renderSkeletonLoading() {
+    return <h1>Loading...</h1>;
+  }
 
   return (
     <div>
-      {users.map((user) => (
-        <div key={user.id} style={{ border: `${pixels} solid black` }}>
-          <div>{user.id}</div>
-          <div>{user.name}</div>
-          <div>{user.email}</div>
-          <div>{user.username}</div>
-        </div>
-      ))}
+      {users.length ? renderUsers() : renderSkeletonLoading()}
     </div>
   );
 }
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,14 +1,43 @@
-import { useParams } from 'react-router-dom'
-import React, { useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Users() {
-    const { username } = useParams()
+  const { username } = useParams(); 
+  const [user, setUser] = useState(null);
 
-useEffect (() => {
-    fetch('')
-}, [])
+  useEffect(() => {
+    async function fetchUser() {
+      const { data } = await axios.get(
+        'https://jsonplaceholder.typicode.com/users'
+      );
 
-return <h1>Users</h1>
+      const foundUser = data.find(
+        (u) => u.username.toLowerCase() === username.toLowerCase()
+      );
+
+      setUser(foundUser || null);
+    }
+
+    fetchUser();
+  }, [username]);
+
+  return (
+    <div>
+      <Link to="/">Go Back</Link>
+
+      {user ? (
+        <>
+          <p>{user.id}</p>
+          <p>{user.name}</p>
+          <p>{user.email}</p>
+          <p>{user.username}</p>
+        </>
+      ) : (
+        <p>User not found</p>
+      )}
+    </div>
+  );
 }
 
 export default Users;
